4d004d32a8d821ebf6d6a5180ec8b46d06fdff17
Edin Sarajlic <edin.sarajlic@canonical.com>
2020-11-13 05:06:39 +0000

    Add Nagios check for Contrail Analytics via introspect

--- contrail-analytics/hooks/contrail_analytics_utils.py
+++ contrail-analytics/hooks/contrail_analytics_utils.py
@@ -336,6 +336,12 @@ def update_nrpe_config():
         check_cmd=common_utils.contrail_status_cmd(MODULE, plugins_dir)
     )
 
+    nrpe_compat.add_check(
+        shortname='contrail_analytics_introspect_status',
+        description='Check contrail-analytics introspect status',
+        check_cmd='check_contrail_introspect.py "http://localhost:8090/Snh_SandeshUVECacheReq?x=NodeStatus"'
+    )
+
     nrpe_compat.write()
 
 
